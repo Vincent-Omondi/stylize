656651941b9c6a3b6fce3f7c3a147ffadfb71aa2
Vincent-Omondi <vincentomondi251@gmail.com>
2024-07-23 17:09:05 +0300

    fix: fix issues with error handling

--- main.go
+++ main.go
@@ -18,10 +18,10 @@ func main() {
 
 	// Use the handler function for routing
 	http.HandleFunc("/", handler)
+	port := ":8080"
+	log.Printf("Server started on http://localhost%s", port)
 
-	log.Println("Server started on http://localhost:8080")
-
-	err := http.ListenAndServe(":8080", nil)
+	err := http.ListenAndServe(port, nil)
 	if errors.Is(err, http.ErrServerClosed) {
 		fmt.Printf("server closed\n")
 	} else if err != nil {
@@ -38,6 +38,6 @@ func handler(w http.ResponseWriter, r *http.Request) {
 	case "/ascii-art":
 		handlers.AsciiArtHandler(w, r)
 	default:
-		http.NotFound(w, r)
+		handlers.ErrorHandler(w, r, http.StatusNotFound, []string{"Page not found"})
 	}
 }
