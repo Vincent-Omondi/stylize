68107fbb94ed8bc5fe5ca5a04919e567fa84868d
Philip <oumaphilip01@gmil.com>
2024-08-05 12:16:27 +0300

    script: add builder.sh to create image, build container and run it

---
+++ builder.sh
@@ -0,0 +1,37 @@
+#!/bin/sh
+
+# # Enable BuildKit for more efficient builds
+# export DOCKER_BUILDKIT=1
+
+# Define variables
+IMAGE_NAME="stylize"
+IMAGE_TAG="1.0"
+CONTAINER_NAME="stylize_container"
+PORT="8080"
+
+# Step 1: Build the Docker image and give it a tag and name
+echo "Building the Docker image..."
+docker build .
+IMAGE_ID=$(docker images -q | head -n 1)
+docker tag ${IMAGE_ID} ${IMAGE_NAME}:${IMAGE_TAG}
+# Check if the build was successful
+if [ $? -eq 0 ]; then
+  echo "Docker image built successfully and named successfully."
+else
+  echo "Docker image build failed."
+  exit 1
+fi
+
+# Step 2: Run the Docker container
+echo "Running the Docker container..."
+docker run -d -p ${PORT}:${PORT} --name ${CONTAINER_NAME} ${IMAGE_NAME}:${IMAGE_TAG}
+
+# Check if the container is running
+if [ $? -eq 0 ]; then
+  echo "Docker container is running."
+else
+  echo "Failed to run the Docker container."
+  exit 1
+fi
+
+echo "Container ${CONTAINER_NAME} is running and accessible on port ${PORT}."
